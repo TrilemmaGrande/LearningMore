655cd4da232f3978b058229f2384304dd7f01e85
Psychokater <psychokater@protonmail.com>
2023-05-05 10:17:56 +0200

    init

---
+++ Aufgabe.SkipAndTake/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Aufgabe.SkipAndTake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0, 22, 12, 16, 18, 11, 19, 13 };
+
+            PrintColl(numbers.Take(5));
+            PrintColl(numbers.TakeLast(5));
+            PrintColl(numbers.Skip(3).SkipLast(3));
+            PrintColl(numbers.TakeWhile(n => n != 22));
+            PrintColl(numbers.SkipWhile(n => n != 12));
+
+            Console.WriteLine(new String('-',80));
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                int itemsPerPage = 5;
+                if (i == 0 || i % itemsPerPage == 0)
+                {
+                    Console.WriteLine("Page: " + ((i / itemsPerPage) + 1) + ":");
+                    Console.WriteLine();
+                    PrintColl(numbers.Skip(i).SkipLast(numbers.Length - i - itemsPerPage));
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine(new String('-', 80));
+
+
+            static void PrintColl<T>(IEnumerable<T> coll)
+            {
+                foreach (T item in coll)
+                {
+                    Console.Write(item);
+                    if (!item.Equals(coll.LastOrDefault()))
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Beispiel.SkipAndTake/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Beispiel.SkipAndTake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Enumerable.Range(1, 50).ToArray();
+
+            int pageSize = 8;
+            for (int page = 1; page <= Math.Ceiling(numbers.Count() / (decimal) pageSize); page++)
+            {
+                Console.WriteLine($"Seite {page}:");
+                PrintColl(numbers
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize));
+            }
+            Console.WriteLine(new String('-',80));
+            PrintColl(numbers.SkipLast(30));
+            PrintColl(numbers.TakeLast(20));
+            Console.WriteLine(new String('-', 80));
+            PrintColl(numbers.TakeWhile(n => n < 20));
+            PrintColl(numbers.SkipWhile(n => n < 20));
+
+            static void PrintColl<T>(IEnumerable<T> coll)
+            {
+                foreach (T item in coll)
+                {
+                    Console.Write(item);
+                    if (!item.Equals(coll.LastOrDefault()))
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
